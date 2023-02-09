b387b14ab676adc58323fd1245de91dc65b2eed1
lanevillec <lanevillec@gmail.com>
2023-02-08 20:28:09 -0500

    fifth try :/

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,37 +2,44 @@
   <div>
     <textarea v-model="inputText"></textarea>
     <button @click="sendText">Send</button>
-    <div>{{response}}</div>
+    <div>{{ response }}</div>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   data() {
     return {
-      inputText: '',
-      response: '',
+      inputText: "",
+      response: "",
     };
   },
   methods: {
     async sendText() {
-    let apiKey = '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';
-    console.log('api key is ' + apiKey);
-      try {
-        const response = await axios.post('https://api.openai.com/v1/completions', {
-          prompt: this.inputText,
-          max_tokens: 100,
-        }, {
-          headers: {
-            'Authorization': `Bearer ${apiKey}`,
+      axios
+        .post(
+          "https://api.openai.com/v1/completions",
+          {
+            model: "text-davinci-003",
+            prompt: this.inputText,
+            max_tokens: 1024,
+            temperature: 0.5,
           },
+          {
+            headers: {
+              Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+            },
+          }
+        )
+        .then(function (response) {
+          console.log(response.data.choices[0].text);
+          this.response = response.data.choices[0].text;
+        })
+        .catch(function (error) {
+          console.error(error);
         });
-        this.response = response.data.choices[0].text;
-      } catch (error) {
-        console.error(error);
-      }
     },
   },
 };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-//import * as dotenv from 'dotenv' 
-//import {Configuration, OpenAIApi} from 'openAI'
 
 createApp(App).mount('#app')
