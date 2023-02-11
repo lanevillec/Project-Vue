5e06c7b0c8e9b17e9dd87ccb03248ea83af471bf
lanevillec <lanevillec@gmail.com>
2023-02-11 13:43:01 -0500

    test 8

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,7 @@
   <div>
     <input v-model="inputText" />
     <button @click="sendInput">Send Input</button>
+    <button @click="getValue">Get Value</button>
   </div>
 </template>
 
@@ -13,12 +14,21 @@ export default {
     };
   },
   methods: {
+    async getValue() {
+      try {
+        const response = await fetch("/.netlify/functions/test-get-value");
+        const data = await response.json();
+        console.log('Success! Was able to get value from netlify function: ' + data);
+      } catch (error) {
+        console.error(error);
+      }
+    },
     async sendInput() {
       try {
         console.log('the input from the user to be sent to netlify function is: ' + this.inputText)
         let bodyToSend = JSON.stringify({ input: this.inputText })
         console.log('bodyToSend to netlify function is: ' + JSON.stringify(bodyToSend))
-        let fullrequest = {
+        /*let fullrequest = {
           method: "POST",
           headers: {
             "Content-Type": "application/json"
@@ -34,7 +44,7 @@ export default {
           body: JSON.stringify({ input: this.inputText })
         });
         const data = await response.json();
-        console.log(data);
+        console.log(data);*/
       } catch (error) {
         console.error('error with sending request to netlify function: ' + error);
       }
