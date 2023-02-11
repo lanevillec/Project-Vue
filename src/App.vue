53746ede99468bb85cdf92e16825fc790333ddda
lanevillec <lanevillec@gmail.com>
2023-02-11 13:56:03 -0500

    test 9

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,49 @@
 <template>
+  <div>
+    <input v-model="inputText" />
+    <button @click="sendInput">Send Input</button>
+    <button @click="getValue">Get Value</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      inputText: ""
+    };
+  },
+  methods: {
+    async getValue() {
+      try {
+        const response = await fetch("/.netlify/functions/test-get-value");
+        const data = await response.json();
+        console.log('Success! Was able to get value from netlify function: ' + data);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async sendInput() {
+      try {
+        console.log('the input from the user to be sent to netlify function is: ' + this.inputText)
+        const response = await fetch("/.netlify/functions/api-call", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({ input: this.inputText })
+        });
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.error('error with sending request to netlify function: ' + error);
+      }
+    }
+  }
+};
+</script>
+
+/*<template>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
@@ -22,4 +67,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-</style>
+</style>*/
+
