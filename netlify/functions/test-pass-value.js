1dcfa0f3511170822c4790594e8d2021d5fab21f
Chris Laneville <chris.laneville@fdmgroup.com>
2023-02-11 20:38:51 -0500

    test release to prod

--- netlify/functions/test-pass-value.js
+++ netlify/functions/test-pass-value.js
@@ -1,12 +1,15 @@
 exports.handler = async (event) => {
-  //const { Configuration, OpenAIApi } = require("openai");
+  const { Configuration, OpenAIApi } = require("openai");
+  //const axios = require('axios');
+  const apiKey = process.env.OPENAI_API_KEY;
   const data = JSON.parse(event.body);
   const inputText = data.message
   console.log('*****data received to netlify function from vue app is: ' + JSON.stringify(data))
   console.log('inputText variable now contains: ' + inputText);
+  console.log('api key retrieved from environment variable is ' + apiKey)
 
-  /*const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+  const configuration = new Configuration({
+    apiKey: apiKey,
   });
   const openai = new OpenAIApi(configuration);
 
@@ -14,14 +17,15 @@ exports.handler = async (event) => {
     model: "text-davinci-003",
     prompt: inputText,
   });
-  console.log(completion.data.choices[0].text);*/
-  //
-  const axios = require('axios');
-  const apiKey = process.env.OPENAI_API_KEY;
+  console.log('got reponse from openai: ' + completion.data.choices[0].text);
 
-  console.log('api key retrieved from environment variable is ' + apiKey)
+  return {
+    statusCode: 200,
+    body: JSON.stringify(completion),
+  }
 
-  try {
+
+  /*try {
     const response = await axios.post('https://api.openai.com/v1/completions', {
       model: "text-davinci-003",
       prompt: inputText,
@@ -43,6 +47,6 @@ exports.handler = async (event) => {
       statusCode: 500,
       body: JSON.stringify({ msg: err.message })
     };
-  }
+  }*/
 
 };
