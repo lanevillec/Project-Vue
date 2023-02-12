8f8e2d585290fc001e2c9841e65a54c16e74981e
Chris Laneville <chris.laneville@fdmgroup.com>
2023-02-11 20:57:48 -0500

    test

--- src/App.vue
+++ src/App.vue
@@ -41,7 +41,7 @@ export default {
           message: this.inputText
         });
         console.log('Response received from netlify function test-past-value is: ' + JSON.stringify(response))
-        let message = JSON.stringify(response)
+        let message = JSON.stringify(response.data)
         this.message = message
       } catch (err) {
         console.error(err);
